c43d3acba33e5228ec89fcd4205dd49189a06c39
Jeff Small <jeffmeister1990@gmail.com>
2022-05-27 19:04:19 -0400

    Wrote out pseudo code, structured directory and files, and created .gitignore

---
+++ develop/assets/script.js
@@ -0,0 +1,19 @@
+//When I click the start button
+
+//A timer starts and I am presented with a question
+
+//When I answer a question
+
+//I am presented with another question
+
+//When I answer a question incorrectly
+
+//Then time is subtracted from teh clock
+
+//When all questions are answered or the time reaches 0
+
+//Then the game is over
+
+//When the game is over
+
+//Then I can save my initials and score
