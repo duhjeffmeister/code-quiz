0771a4ec3dd07fb2c2d11852136441f08e3b3e64
Jeff Small <jeffmeister1990@gmail.com>
2022-05-28 01:57:13 -0400

    Finished basic HTML elements.

--- develop/assets/script.js
+++ develop/assets/script.js
@@ -1,19 +1,51 @@
+//Declare all variables
+var userInitials = ""
+var highScore = ""
+var questionOne =
+var questionTwo = 
+var questionThree = 
+var questionFour = 
+var questionFive = 
+
+
+//Start button
+
+//Declare timer variable for 60 seconds
+
+//Declare high score
+
+//Question 1
+    //Array with answers
+
+//Question 2
+    //Array with answers
+
+//Question 3
+    //Array with answers
+
+//Question 4
+    //Array with answers
+
+//Question 5
+    //Array with answers
+
+
 //When I click the start button
+    //Create start button with event listener
+    //A timer starts and I am presented with a question
+    
+    //When I answer a question
+        //Create an event listener for the various answers
 
-//A timer starts and I am presented with a question
+    //I am presented with another question
+        //Bring up the second question, etc.
 
-//When I answer a question
+    //When I answer a question incorrectly
+        //Then time is subtracted from the clock
 
-//I am presented with another question
+    //When all questions are answered or the time reaches 0
+        //Then the game is over
 
-//When I answer a question incorrectly
-
-//Then time is subtracted from teh clock
-
-//When all questions are answered or the time reaches 0
-
-//Then the game is over
-
-//When the game is over
-
-//Then I can save my initials and score
+    //When the game is over
+        //Then I can save my initials and score
+        //Save score array to localstorage.
